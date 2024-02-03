fix bug in test loss computation in analysis/loss

analysis/loss.py
@@ -2,6 +2,7 @@ from optimizer.sam import disable_running_stats, enable_running_stats
 import torch
 from tqdm import tqdm
 import torch.nn.functional as F
+import sys
 
 def compute_loss(graphs, model, loss_name, criterion, criterion_summed, device, num_classes, loader_abridged, test_loader):
     disable_running_stats(model)
@@ -12,7 +13,7 @@ def compute_loss(graphs, model, loss_name, criterion, criterion_summed, device, 
         data, target = data.to(device), target.to(device)
         out = model(data)
         if str(loss_name) == 'CrossEntropyLoss':
-            loss = criterion(out, target)
+            loss = criterion_summed(out, target)
         elif str(loss_name) == 'MSELoss':
             #print(out[0], target[0])
             #loss = criterion(out, F.one_hot(target, num_classes=num_classes).float()) * num_classes
@@ -34,9 +35,9 @@ def compute_loss(graphs, model, loss_name, criterion, criterion_summed, device, 
     for batch_idx, (data, target) in enumerate(test_loader, start=1):
         data, target = data.to(device), target.to(device)
         out = model(data)
-        if str(criterion) == 'CrossEntropyLoss':
-            loss = criterion(out, target)
-        elif str(criterion) == 'MSELoss':
+        if str(loss_name) == 'CrossEntropyLoss':
+            loss = criterion_summed(out, target)
+        elif str(loss_name) == 'MSELoss':
             #print(out[0], target[0])
             #loss = criterion(out, F.one_hot(target, num_classes=num_classes).float()) * num_classes
             #if transform_to_one_hot:
@@ -45,6 +46,7 @@ def compute_loss(graphs, model, loss_name, criterion, criterion_summed, device, 
             loss = criterion_summed(out, target)
         if out.dim() > 1:
             accuracy = torch.sum((torch.argmax(out,dim=1)==target).float()).item()
+            wrong_index = torch.where((torch.argmax(out,dim=1)==target).float() == 0)[0]
 
         pbar.update(1)
         pbar.set_description(