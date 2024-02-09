635480cb9ce9b3c858390ac302e4c4849ab91a3e
Chen <lucmon@illinois.edu>
2024-02-08 18:21:16 -0600

    weight norm analysis for wide resnet with weight norm layer

--- analysis/weight_norm.py
+++ analysis/weight_norm.py
@@ -1,13 +1,5 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import torch
 #from sklearn.preprocessing import normalize
-import scipy
-import json
-from torch import nn
-from torch.nn.utils import parameters_to_vector, weight_norm
-from torch.utils.data import DataLoader
-import torch.nn.functional as F
 
 def get_min_weight_norm(graphs, model, C, model_name):
     norm_min = 1e10
@@ -24,8 +16,16 @@ def get_min_weight_norm(graphs, model, C, model_name):
             if 'output_layer' in name:
                 continue
             norm_min = min(norm_min, torch.min(torch.norm(param, dim=-1)).cpu().detach().numpy())
-        if model_name == 'weight_norm_torch' and 'weight_v' in name:
-            norm_min = min(norm_min, torch.min(torch.norm(param, dim=-1)).cpu().detach().numpy())
+        elif model_name in ['weight_norm_torch']:
+            if 'weight_v' in name:
+                norm_min = min(norm_min, torch.min(torch.norm(param, dim=-1)).cpu().detach().numpy())
+        elif model_name in ['WideResNet_WN_woG']:
+            if 'weight_v' in name:
+                assert param.ndim == 4
+                param_norm = torch.norm(param, dim=[1,2])
+                norm_min = min(norm_min, torch.min(torch.norm(param_norm, dim=-1)).cpu().detach().numpy())
+        else:
+            raise NotImplementedError
     graphs.wn_norm_min.append(norm_min)
 
 def get_grad_loss_ratio(graphs, model, loss_name, loader, criterion, criterion_summed, num_classes, device):
@@ -34,20 +34,17 @@ def get_grad_loss_ratio(graphs, model, loss_name, loader, criterion, criterion_s
     for batch_idx, (data, target) in enumerate(loader, start=1):
         data, target = data.to(device), target.to(device)
         out = model(data)
-        if loss_name == 'CrossEntropyLoss':
-            loss = criterion_summed(out, target)
-        elif loss_name == 'MSELoss':
-            #print(out[0], target[0])
-            #loss = criterion(out, F.one_hot(target, num_classes=num_classes).float()) * num_classes
-            #loss = criterion_summed(out, F.one_hot(target, num_classes=num_classes).float()).float()
-            loss = criterion_summed(out, target)
-            #print(loss)
+        #if loss_name == 'CrossEntropyLoss':
+        loss = criterion_summed(out, target)
+        #elif loss_name == 'MSELoss':
+        #loss = criterion_summed(out, target)
         loss_sum += loss
     loss_mean = loss_sum / len(loader.dataset)
     loss_mean.backward()
     grad_norm = 0
     for param in model.parameters():
-        grad_norm += torch.norm(param.grad)**2
+        if param.grad is not None:
+            grad_norm += torch.norm(param.grad)**2
     graphs.wn_grad_loss_ratio.append((grad_norm/loss_mean).item())
     print(graphs.wn_grad_loss_ratio[-1])
     print(graphs.wn_norm_min[-1])
