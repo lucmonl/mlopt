cd2b5129f76dcb79f505bb6cd8b9e75567e2eeec
Chen <lucmon@illinois.edu>
2024-02-10 10:01:04 -0600

    save & load optimizer state_dict

--- notebook/plot_functions.py
+++ notebook/plot_functions.py
@@ -33,7 +33,7 @@ def plot_figures_opts(opts, model_params, opt_params):
 
 
         cur_epochs = train_graphs.log_epochs
-        plt.subplot(2,5,1)
+        plt.subplot(2,6,1)
         plt.semilogy(cur_epochs, train_graphs.loss)
         #plt.legend(['Loss + Weight Decay'])
         plt.xlabel('Epoch')
@@ -41,7 +41,7 @@ def plot_figures_opts(opts, model_params, opt_params):
         plt.title('Training Loss')
 
 
-        plt.subplot(2,5,2)
+        plt.subplot(2,6,2)
         print(train_graphs.eigs)
         plt.semilogy(cur_epochs, train_graphs.eigs)
         #plt.legend(['Loss + Weight Decay'])
@@ -49,27 +49,33 @@ def plot_figures_opts(opts, model_params, opt_params):
         plt.ylabel('Value')
         plt.title('Sharpness')
 
-        plt.subplot(2,5,3)
+        plt.subplot(2,6,3)
         plt.semilogy(cur_epochs, train_graphs.test_loss)
         #.legend(['Loss + Weight Decay'])
         plt.xlabel('Epoch')
         plt.ylabel('Value')
         plt.title('Testing Loss')
 
-        plt.subplot(2,5,4)
+        plt.subplot(2,6,4)
         plt.semilogy(cur_epochs, train_graphs.test_accuracy)
         #.legend(['Loss + Weight Decay'])
         plt.xlabel('Epoch')
         plt.ylabel('Value')
         plt.title('Testing Accuracy')
 
-        plt.subplot(2,5,5)
+        plt.subplot(2,6,5)
         plt.plot(cur_epochs, train_graphs.eigs_test)
-        print(train_graphs.eigs_test)
         #.legend(['Loss + Weight Decay'])
         plt.xlabel('Epoch')
         plt.ylabel('Value')
         plt.title('Testing Sharpness')
+
+        plt.subplot(2,6,6)
+        plt.plot(cur_epochs, train_graphs.adv_eigs)
+        #.legend(['Loss + Weight Decay'])
+        plt.xlabel('Epoch')
+        plt.ylabel('Value')
+        plt.title('Adversarial Sharpness')
 
     plt.legend(opts)
     plt.tight_layout()
